4d21aae3fa644e250398513eab5ae6619bc7a508
aqsa13 <90684765+aqsa13@users.noreply.github.com>
2026-08-31 22:40:20 +0200

    code refinement

--- basic_environment.py
+++ basic_environment.py
@@ -2,14 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-ARENA_SIZE = 100.0 
-NUMBER_OF_ROBOTS = 50
-STEP_SIZE= 1.0
-DETECTION_RADIUS = 5.0
-NUMBER_OF_STEPS = 1.0
-RESOURCE_SITE  = np.array([75.0, 75.0]) 
-RNG_SEED = 42
-rng=np.random.default_rng(RNG_SEED)
+import params as p
 
 def rand_robo_placement(number_of_robots, arena_size, rng):
     return rng.uniform(0, arena_size, size=(number_of_robots, 2))
@@ -28,13 +21,13 @@ def site_detected(positions, site_position, detection_radius):
     else: 
         return False
 def run_base_environment():
-    positions = rand_robo_placement(NUMBER_OF_ROBOTS, ARENA_SIZE, rng)
-    has_discovered = np.zeros(NUMBER_OF_ROBOTS, dtype=bool)
-    found_ratio = np.zeros(NUMBER_OF_STEPS)
+    positions = rand_robo_placement(p.NUMBER_OF_ROBOTS, p.ARENA_SIZE, p.rng)
+    has_discovered = np.zeros(p.NUMBER_OF_ROBOTS, dtype=bool)
+    found_ratio = np.zeros(p.NUMBER_OF_STEPS)
     trajectory = []
-    for traj in range(NUMBER_OF_STEPS):
-        positions = walk(positions, STEP_SIZE, ARENA_SIZE, rng)
-        curr_in_range = site_detected(positions, RESOURCE_SITE, DETECTION_RADIUS)
+    for traj in range(p.NUMBER_OF_STEPS):
+        positions = walk(positions, p.STEP_SIZE, p.ARENA_SIZE, p.rng)
+        curr_in_range = site_detected(positions, p.RESOURCE_SITE, p.DETECTION_RADIUS)
         has_discovered = has_discovered | curr_in_range
         found_ratio[traj] = has_discovered.mean()
         trajectory.append(positions[0].copy())
